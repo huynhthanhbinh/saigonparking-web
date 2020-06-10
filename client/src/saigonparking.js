08fd4c70c6776ac117a21020ce779c89face5ef7
1653005 <1653005@student.hcmus.edu.vn>
2020-06-10 20:45:37 +0700

    Finish Config host

--- client/src/saigonparking.js
+++ client/src/saigonparking.js
@@ -1,3 +1,5 @@
 export const API_URL = (process.env.NODE_ENV === 'production')
     ? "https://saigonparking.wtf:8448"
     : "http://localhost:8338";
+
+// export const API_URL = "http://localhost:8338";
