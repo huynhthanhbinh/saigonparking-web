f452a5e81a4d4952b46a47f79212db409d1aa48b
1653020 <46862344+1653020@users.noreply.github.com>
2020-07-07 01:45:53 +0700

    complete send and received messeage (websocket)

--- parkinglot/src/App.js
+++ parkinglot/src/App.js
@@ -8,11 +8,10 @@ import 'semantic-ui-css/semantic.min.css'
 import authProto from './api/Auth_pb'
 import { API_URL } from './saigonparking'
 import { AuthServiceClient } from './api/Auth_grpc_web_pb'
+import contactProto from './api/Contact_pb'
 
 const userProto = require('./api/Actor_pb')
 const authService = new AuthServiceClient(API_URL)
-
-// const clients = new W3CWebSocket('ws://localhost:8000/contact', 'echo-protocol', '');
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
@@ -21,8 +20,7 @@ function App() {
   const [flagIsLogin, setFlagIsLogin] = useState(false)
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
-  // const clients = new W3CWebSocket()
-  // const [client, setClient] = useState(new W3CWebSocket('ws://localhost:8000/contact', 'echo-protocol', '', token))
+  const [messageReceived, setMesssageReceived] = useState()
 
   const handleChange = (e) => {
     setValue(e.target.value)
@@ -33,27 +31,48 @@ function App() {
       setLists(lists.concat(data));
     }
     console.log(lists)
+    console.log(messageReceived)
   }
 
   useEffect(() => {
-    
+
     const token = Cookies.get("token");
     const refreshtoken = Cookies.get("refreshtoken");
     const checkUserName = Cookies.get("checkUserName");
     if (token && checkUserName && refreshtoken) {
       setFlagIsLogin(true)
       const clients = new W3CWebSocket(`ws://localhost:8000/contact/web?token=${token}`);
+
       clients.onerror = function (error) {
         console.log(error);
       }
+
       clients.onclose = (event) => {
         console.log(event)
       }
+
       clients.onopen = () => {
         console.log('WebSocket Client Connected');
+
+        // sendMessage set filed and send //
+        const message = new contactProto.SaigonParkingMessage()
+        message.setSenderid(14)
+        message.setReceiverid(4)
+        message.setContent('abcd 12345674989876')
+        message.setClassification(contactProto.SaigonParkingMessage.Classification.PARKING_LOT_MESSAGE)
+        message.setType(contactProto.SaigonParkingMessage.Type.TEXT_MESSAGE)
+        clients.send(message.serializeBinary())
+        // ------------------------------------------------------------------------ //
+
       };
-      clients.onmessage = (message) => {
-        console.log(JSON.parse(message.data).message);
+
+      clients.onmessage = (data) => {
+        // change received message to Unit8Array and convert to object and set to State MesssageReceived //
+        data.data.arrayBuffer().then(function (v) {
+          var buf = new Uint8Array(v)
+          setMesssageReceived(contactProto.SaigonParkingMessage.deserializeBinary(buf))
+        })
+        // ------------------------------------------------------------------------ //
       };
     }
     else {
