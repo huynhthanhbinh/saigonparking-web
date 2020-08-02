3c89eb9683191c9fac8f962a6ee3158d23dbdd71
1653020 <46862344+1653020@users.noreply.github.com>
2020-08-02 22:50:58 +0700

    update api 2.3.7

--- admin/src/api/Auth_grpc_web_pb.js
+++ admin/src/api/Auth_grpc_web_pb.js
@@ -164,6 +164,86 @@ proto.com.bht.saigonparking.api.grpc.auth.AuthServicePromiseClient.prototype.che
 /**
  * @const
  * @type {!grpc.web.MethodDescriptor<
+ *   !proto.google.protobuf.StringValue,
+ *   !proto.google.protobuf.BoolValue>}
+ */
+const methodDescriptor_AuthService_checkEmailAlreadyExist = new grpc.web.MethodDescriptor(
+  '/com.bht.saigonparking.api.grpc.auth.AuthService/checkEmailAlreadyExist',
+  grpc.web.MethodType.UNARY,
+  google_protobuf_wrappers_pb.StringValue,
+  google_protobuf_wrappers_pb.BoolValue,
+  /**
+   * @param {!proto.google.protobuf.StringValue} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  google_protobuf_wrappers_pb.BoolValue.deserializeBinary
+);
+
+
+/**
+ * @const
+ * @type {!grpc.web.AbstractClientBase.MethodInfo<
+ *   !proto.google.protobuf.StringValue,
+ *   !proto.google.protobuf.BoolValue>}
+ */
+const methodInfo_AuthService_checkEmailAlreadyExist = new grpc.web.AbstractClientBase.MethodInfo(
+  google_protobuf_wrappers_pb.BoolValue,
+  /**
+   * @param {!proto.google.protobuf.StringValue} request
+   * @return {!Uint8Array}
+   */
+  function(request) {
+    return request.serializeBinary();
+  },
+  google_protobuf_wrappers_pb.BoolValue.deserializeBinary
+);
+
+
+/**
+ * @param {!proto.google.protobuf.StringValue} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @param {function(?grpc.web.Error, ?proto.google.protobuf.BoolValue)}
+ *     callback The callback function(error, response)
+ * @return {!grpc.web.ClientReadableStream<!proto.google.protobuf.BoolValue>|undefined}
+ *     The XHR Node Readable Stream
+ */
+proto.com.bht.saigonparking.api.grpc.auth.AuthServiceClient.prototype.checkEmailAlreadyExist =
+    function(request, metadata, callback) {
+  return this.client_.rpcCall(this.hostname_ +
+      '/com.bht.saigonparking.api.grpc.auth.AuthService/checkEmailAlreadyExist',
+      request,
+      metadata || {},
+      methodDescriptor_AuthService_checkEmailAlreadyExist,
+      callback);
+};
+
+
+/**
+ * @param {!proto.google.protobuf.StringValue} request The
+ *     request proto
+ * @param {?Object<string, string>} metadata User defined
+ *     call metadata
+ * @return {!Promise<!proto.google.protobuf.BoolValue>}
+ *     A native promise that resolves to the response
+ */
+proto.com.bht.saigonparking.api.grpc.auth.AuthServicePromiseClient.prototype.checkEmailAlreadyExist =
+    function(request, metadata) {
+  return this.client_.unaryCall(this.hostname_ +
+      '/com.bht.saigonparking.api.grpc.auth.AuthService/checkEmailAlreadyExist',
+      request,
+      metadata || {},
+      methodDescriptor_AuthService_checkEmailAlreadyExist);
+};
+
+
+/**
+ * @const
+ * @type {!grpc.web.MethodDescriptor<
  *   !proto.com.bht.saigonparking.api.grpc.auth.ValidateRequest,
  *   !proto.com.bht.saigonparking.api.grpc.auth.ValidateResponse>}
  */
