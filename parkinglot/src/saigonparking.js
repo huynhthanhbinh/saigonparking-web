7b256221d5d34c2d39356c63d12f4729b53de70a
1653020 <46862344+1653020@users.noreply.github.com>
2020-08-19 21:41:03 +0700

    Product

--- parkinglot/src/saigonparking.js
+++ parkinglot/src/saigonparking.js
@@ -1,9 +1,9 @@
 export const API_URL = (process.env.NODE_ENV === 'production')
     ? "https://saigonparking.wtf:8448"
-    : "http://ylas2712.ddns.net:8338";
+    : "http://localhost:8338";
 
 export const WEBSOCKET_URL = (process.env.NODE_ENV === 'production')
     ? "wss://saigonparking.wtf:8443"
-    : "ws://ylas2712.ddns.net:8000";
+    : "ws://localhost:8000";
 
 // export const API_URL = "http://localhost:8338";
